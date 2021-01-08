5972b6fd45a3b3cddec77821ce82e21dd5cd8295
Kai <ksah466@aisbmali.org>
2021-01-08 18:21:33 +0000

    Finished starting screen

--- alphabet/main.js
+++ alphabet/main.js
@@ -1,3 +1,5 @@
+const devMode = false;
+
 let startQueue = [documentLoaded]
 
 let slider = {
@@ -5,25 +7,49 @@ let slider = {
     total: startQueue.length,
     add() {
         slider.current += 1;
-        document.getElementById("slider").setAttribute("value", (100 * slider.current) / slider.total)
+        document.querySelector("#start progress").setAttribute("value", (100 * slider.current) / slider.total)
     }
 }
 
-function delay() {
-    return new Promise(resolve => setTimeout(() => resolve(), 500))
+function delay(ms) {
+    return new Promise(resolve => setTimeout(() => resolve(), ms))
 }
 
 async function documentLoaded() {
     await new Promise((resolve) => {
         window.addEventListener('load', () => resolve());
     })
+}
 
-    // Fix loading animations
-    document.querySelector("body").classList.remove("preload")
+let user = {
+    country: "ml",
+    get flag() {
+        return `https://www.countryflags.io/${user.country}/flat/64.png`
+    }
+}
+
+async function getCountry() {
+    let location = await fetch("https://extreme-ip-lookup.com/json/").then(res => res.json())
+    user.country = location.countryCode.toLowerCase();
 }
 
 // Main method
 Promise.all(startQueue.map(asyncFunction => asyncFunction().then(slider.add)))
     .then(() => {
-        document.getElementById("start").disabled = false;
+        // Fix loading animations
+        document.querySelector("body").classList.remove("preload")
+
+        document.querySelector("#start button").disabled = false;
+        document.querySelector("#start button").onclick = transitionToCategories
     })
+
+async function transitionToCategories() {
+    document.querySelector("#start").style.opacity = 0;
+    if (!devMode) await delay(250);
+    document.querySelector("#start").remove()
+    document.querySelector("#categories").style.display = "";
+}
+
+if (devMode) {
+    transitionToCategories()
+}
